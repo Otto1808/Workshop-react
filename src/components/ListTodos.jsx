19b645383e9360b62a67493cf9e981da05f878b8
Pietro Scaduto <pietroscaduto@MacBook-Air-de-Pietro.local>
2023-07-14 14:12:54 +0200

    Test deploys

--- src/components/ListTodos.jsx
+++ src/components/ListTodos.jsx
@@ -11,7 +11,7 @@ export default function ListTodos({todos, changeTodoStatus, deleteTodo}) {
       <section>
         <h2>Todos</h2>
         <ul>
-          {todos.map((todo, index) => (
+          {todos && todos.map((todo, index) => (
             // eslint-disable-next-line react/jsx-key
             <li key={index}>
               <input type="checkbox" 
