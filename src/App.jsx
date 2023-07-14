2c0d59797caba85bf61aa1e8c8161092e00142b5
Pietro Scaduto <pietroscaduto@MacBook-Air-de-Pietro.local>
2023-07-14 14:17:44 +0200

    Fix localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
 
   useEffect (() => {
     const initialTodos = JSON.parse(window.localStorage.getItem(LSKEY + '.todos'))
-    setTodos(initialTodos);
+    setTodos(initialTodos && []);
   }, []);
 
   useEffect( () => {
